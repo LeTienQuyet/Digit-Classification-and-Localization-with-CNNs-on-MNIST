ff483317898b1bed8708c03d1322d1dbfc4727c7
LeTienQuyet <21520428@gm.uit.edu.vn>
2024-11-18 16:26:29 +0700

    make predict for real paint

--- src/demo.py
+++ src/demo.py
@@ -1,16 +1,24 @@
 from tkinter import colorchooser, messagebox
-from PIL import ImageGrab
+from PIL import ImageGrab, Image
+from predict import load_model
+from train import make_transform
 
+import torch
 import tkinter as tk
+import matplotlib.pyplot as plt
 
 class PaintApp:
-    def __init__(self, root, bg_color="black", text_color="white", width=400, height=400, size=5):
+    def __init__(self, root, bg_color="black", text_color="white", width=420, height=420, size=3):
         self.root = root
         self.bg_color = bg_color
         self.text_color = text_color
         self.size = size
         self.width = width
         self.height = height
+
+        self.model = load_model(out_channels=10, path_to_checkpoint="../checkpoint")
+        self.scaleX = self.width / 70
+        self.scaleY = self.height / 70
 
         self.root.title("Predict digit with Tkinter")
 
@@ -23,7 +31,7 @@ class PaintApp:
         btn_clear = tk.Button(self.root, text="Clear", command=self.clear_canvas)
         btn_clear.pack(side=tk.LEFT, expand=True)
 
-        btn_save = tk.Button(self.root, text="Predict", command=self.save_canvas)
+        btn_save = tk.Button(self.root, text="Predict", command=self.make_predict)
         btn_save.pack(side=tk.LEFT, expand=True)
 
     def paint(self, event):
@@ -37,15 +45,23 @@ class PaintApp:
     def clear_canvas(self):
         self.canvas.delete("all")
 
-    def save_canvas(self):
+    def make_predict(self):
         xmin = self.root.winfo_rootx() + self.canvas.winfo_x()
         ymin = self.root.winfo_rooty() + self.canvas.winfo_y()
         xmax = xmin + self.canvas.winfo_width()
         ymax = ymin + self.canvas.winfo_height()
 
-        image = ImageGrab.grab((xmin, ymin, xmax, ymax))
-        # x_out, y_out, w_out, h_out = 20, 20, 100, 100
-        # self.canvas.create_rectangle(x_out, y_out, x_out+w_out, y_out+h_out, fill=None, outline="red")
+        image = ImageGrab.grab((xmin, ymin, xmax, ymax)).convert("L")
+        img = make_transform()(image)
+        img = img.unsqueeze(0)
+
+        self.model.eval()
+        with torch.no_grad():
+            class_logits, bbox_out = self.model(img)
+            class_out = torch.argmax(class_logits, dim=1)
+            x, y, w, h = bbox_out[0].cpu().numpy()
+            self.canvas.create_rectangle(self.scaleX*x, self.scaleY*y, self.scaleX*(x+w), self.scaleY*(y+h), fill=None, outline="red")
+            self.canvas.create_text(self.scaleX*(x+w/2), self.scaleY*(y)-12, text=f"{class_out.item()}", font=("Arial, 15"), fill="red")
 
 if __name__ == "__main__":
     root = tk.Tk()
